2571293f5709bd142e1e2162f02e3e7c304b3d63
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-27 15:53:19 -0700

    PASS Robot set 1
    Co-authored-by: iamalion <iamalion@users.noreply.github.com>

--- src/js/bs/Robot.js
+++ src/js/bs/Robot.js
@@ -2,6 +2,6 @@
 export default class Robot {
     constructor(dino, set = '') {
         this.dino = dino;
-        this.src = `https://robohash.org/${this.dino}${set ? '?set=set' + set : ''}`;
+        this.src = `https://robohash.org/${this.dino}${set && set !== 1 ? '?set=set' + set : ''}`;
     }
 }
