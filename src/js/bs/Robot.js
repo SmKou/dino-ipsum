ccdf71e0ac7255551cc11786490fdf9375bfaadb
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-27 15:41:53 -0700

    PASS Robot set
    Co-authored-by: iamalion <iamalion@users.noreply.github.com>

--- src/js/bs/Robot.js
+++ src/js/bs/Robot.js
@@ -1,8 +1,7 @@
-/* Turn a dinosaur from dino ipsum into a robot from robohash
- ${set ? '?set=set' + set : ''}*/
+/* Turn a dinosaur from dino ipsum into a robot from robohash*/
 export default class Robot {
     constructor(dino, set = '') {
         this.dino = dino;
-        this.src = `https://robohash.org/${this.dino}`;
+        this.src = `https://robohash.org/${this.dino}${set ? '?set=set' + set : ''}`;
     }
 }
